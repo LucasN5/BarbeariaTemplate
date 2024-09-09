0109265bcbd4bf8d742ace97ae827c56f6e5ea0a
LucasN5 <lucasxben@hotmail.com>
2024-09-09 19:44:47 -0300

    adds Hero section and NavBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-background": "url('../assets/heroBackground.jpg')",
+        "price-background": "url('../assets/priceBackground.jpg')",
+      },
+
+      fontFamily: {
+        Raleway: ["Raleway", "sans-serif"],
+        NotoSans: ["Noto+Sans", "sans-serif"],
+      },
+
+      colors: {
+        primary: "#181818",
+        "primary-light": "#242322",
+        secondary: "#FBB034",
+        "secondary-light": "#D5A353",
+        white: "#fcfcfc",
+      },
+    },
   },
   plugins: [],
 };
