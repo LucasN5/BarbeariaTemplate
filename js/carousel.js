a9bbe8f083bdbea63e9f1d3d4243700656d48de5
LucasN5 <lucasxben@hotmail.com>
2024-10-18 15:07:47 -0300

    makes both slides from 	estimonials and 	eams functionable

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Splide(".splide-teams", {
-    loop: true,
-    rewind: true,
-    rewindByDrag: true,
-    perPage: 3,
-    perMove: 1,
+    type: "loop", // Slide simples
+    perPage: 3, // Mostra 3 slides por vez
+    perMove: 3, // Move 1 slide por vez
+
+    pagination: true, // Desativa a paginação
+    arrows: true, // Habilita as setas de navegação
+    drag: true, // Desativa o arrasto livre
+    direction: "ltr", // Permite navegação apenas da esquerda para a direita
+    classes: {
+      page: "splide__pagination__page splide_customization",
+    },
+    breakpoints: {
+      1024: {
+        perPage: 2, // Ajuste para tablets
+      },
+      768: {
+        perPage: 1, // Ajuste para mobile
+      },
+    },
   }).mount();
 
   new Splide(".splide-testimonials", {
-    loop: true,
-    rewind: true,
-    rewindByDrag: true,
-    perPage: 2,
-    perMove: 1,
+    type: "loop", // Slide simples
+    perPage: 2, // Mostra 3 slides por vez
+    perMove: 1, // Move 1 slide por vez
+
+    pagination: false, // Desativa a paginação
+    arrows: true, // Habilita as setas de navegação
+    drag: true, // Desativa o arrasto livre
+    direction: "ltr", // Permite navegação apenas da esquerda para a direita
+    classes: {
+      page: "splide__pagination__page splide_customization",
+    },
+    breakpoints: {
+      1024: {
+        perPage: 2, // Ajuste para tablets
+      },
+      768: {
+        perPage: 1, // Ajuste para mobile
+      },
+    },
   }).mount();
 });
