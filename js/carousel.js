55c726262399dbc64d6de9f788999656e1c9b603
LucasN5 <lucasxben@hotmail.com>
2024-10-22 19:36:36 -0300

    adds 	estimonials slide customization, adds customization on prev and next butto from the slides

--- js/carousel.js
+++ js/carousel.js
@@ -24,17 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   new Splide(".splide-testimonials", {
     type: "loop", // Slide simples
-    perPage: 2, // Mostra 3 slides por vez
+    perPage: 1, // Mostra 3 slides por vez
     perMove: 1, // Move 1 slide por vez
 
     pagination: false, // Desativa a paginação
     arrows: true, // Habilita as setas de navegação
     drag: true, // Desativa o arrasto livre
-    direction: "ltr", // Permite navegação apenas da esquerda para a direita
-
+    classes: {
+      prev: "splide__arrow--prev splide__testimonials__arrow",
+      next: "splide__arrow--next splide__testimonials__arrow",
+    },
     breakpoints: {
       1024: {
-        perPage: 2, // Ajuste para tablets
+        perPage: 1, // Ajuste para tablets
       },
       768: {
         perPage: 1, // Ajuste para mobile
